0e317f57bca8361fcb19497eab3de07fe1423abf
SergioSaulAM <ssaulam34@gmail.com>
2022-04-22 18:39:48 -0500

    Agregando clase y archivo index.js y jest

---
+++ weekly_mission_3/my_first_js_project/index.js
@@ -0,0 +1,5 @@
+//Archivo principal de todo lo que vaya creando en los folders y la estructura que deseo hacer
+
+const Ajolonauta = require('./app/Ajolonauta');
+const woopa = new Ajolonauta("Woopa");
+console.log(woopa);
