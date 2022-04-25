f77ccc7e40ef136874021aaa4c74ac25d26441b7
SergioSaulAM <ssaulam34@gmail.com>
2022-04-25 15:58:23 -0500

    Agregando clase Spiderman del directorio App

---
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class SpiderMan {
+    constructor(name, age, actor, movies, studio) {
+        this.name = name;
+        this.age = age;
+        this.actor = actor;
+        this.movies = movies;
+        this.studio = studio;
+    }
+}
+
+module.exports = SpiderMan;
